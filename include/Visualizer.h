57d65c737313380c6314ca1fde9f4aa386f19864
Grant LeRette <grantjlk@gmail.com>
2025-12-06 21:01:35 -0600

    Move fuction definitions to .cpp

--- include/Visualizer.h
+++ include/Visualizer.h
@@ -1,3 +1,6 @@
+#ifndef VISUALIZER_H
+#define VISUALIZER_H
+
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <vector>
@@ -49,29 +52,4 @@ class Visualizer{
     	Visualizer(const Visualizer&) = delete;
     	Visualizer& operator=(const Visualizer&) = delete;
 };
-
-Visualizer::~Visualizer(){
-	
-}
-bool Visualizer::setupWindow(){
-	
-}
-bool Visualizer::setupShaders(){
-	
-}
-bool Visualizer::setupGeometry(){
-	
-}
-static void framebufferSizeCallback(GLFWwindow* window, int width, int height){
-	glViewport(0, 0, width, height);
-	//update stored dimensions? 
-}
-
-bool Visualizer::initialize(){
-	// initialize GLFW
-    if (!glfwInit()) {
-		std::cerr << "GLFW failed to initialize\n";
-		return false;
-	}
-	setupWindow;
-}
+#endif
